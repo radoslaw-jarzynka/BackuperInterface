0b54e860993fdf96b8025dc6348147a59786149b
Radosław Jarzynka <radoslaw.jarzynka@gmail.com>
2013-11-25 17:19:16 +0100

    ver2

--- BackuperInterface/src/common/BackuperInterface.java
+++ BackuperInterface/src/common/BackuperInterface.java
@@ -14,10 +14,9 @@ import com.healthmarketscience.rmiio.RemoteInputStream;
 
 public interface BackuperInterface extends Remote {
 
-	public void uploadFile(String username, String fileName, long lastModified, String md5hex, RemoteInputStream remoteFileData) throws RemoteException;
+	public void uploadFile(String username, String fileName, long lastModified, RemoteInputStream remoteFileData) throws RemoteException;
 	public RemoteInputStream getFile(String username, String fileName) throws RemoteException;
 	public String getFileMD5(String username, String fileName) throws RemoteException;
-	public long getFileLastModified (String username, String fileName) throws RemoteException;
 	public Vector<String> getListOfFilesOnServer(String username) throws RemoteException;
 	public HashMap<String,Long> getMapOfFilesOnServer(String username) throws RemoteException;
 	public boolean logIn(String username, String password) throws RemoteException;
